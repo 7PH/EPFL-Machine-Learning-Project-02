f8bf20c3789a092b221664cdd0d454597482fdb9
PowerHat <b.raymond@protonmail.com>
2018-11-23 01:04:42 +0100

    Finish setup scripts for downloading datasets

--- bash_npm/setup.sh
+++ bash_npm/setup.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-echo "@TODO build setup script" && exit 1;
+echo "Downloading datasets..";
+mkdir data;
+cd data;
+wget http://uploads.benjamin-raymond.pro/2018/11/23/01-24-01-000-sample_submission.csv -O sample_submission.csv;
+wget http://uploads.benjamin-raymond.pro/2018/11/23/01-24-01-001-test_data.txt -O test_data.txt;
+wget http://uploads.benjamin-raymond.pro/2018/11/23/01-24-01-002-twitter-datasets.zip -O twitter-datasets.zip;
+cd ../;
