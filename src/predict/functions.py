aa0ca593d822f893d722b64913b4cf1e63132704
PowerHat <b.raymond@protonmail.com>
2018-12-10 14:40:38 +0100

    Fix prediction

---
+++ src/predict/functions.py
@@ -0,0 +1,5 @@
+
+
+def predict(model, x, no_value=0, yes_value=1, batch_size=128):
+    predicted = model.predict_proba(x, batch_size=batch_size)
+    return [yes_value if pred > .5 else no_value for pred in predicted]
